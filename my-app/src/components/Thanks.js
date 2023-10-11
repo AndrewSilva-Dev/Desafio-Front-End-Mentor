62fa268d89c87e99182b6be5b8c6f10159b2a303
AndrewSilva-Dev <147275462+AndrewSilva-Dev@users.noreply.github.com>
2023-10-11 18:45:25 -0300

    Update Thanks.js

--- my-app/src/components/Thanks.js
+++ my-app/src/components/Thanks.js
@@ -2,17 +2,20 @@ import styles from './Thanks.module.css'
 import imgThanks from '../img/illustration-thank-you.svg'
 
 
-function Thanks({nota}){
-    return(
+function Thanks({ nota }) {
+    return (
         <div className={styles.card}>
             <img src={imgThanks} alt="" />
             <div className={styles.nota}>
                 <p>Sua nota Foi {nota} de 5</p>
-                
+
             </div>
-            <h1>Obrigado!</h1>
+            <div className={styles.agradecimento}>
+                <h1>Obrigado!</h1>
                 <p>Agradecemos por dedicar seu tempo para avaliar.</p>
                 <p>Se precisar de mais suporte,</p>
+                <p>não hesite em entrar em contato</p>
+            </div>
         </div>
     )
 }
